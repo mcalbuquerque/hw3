a0363046be656290de3bcfb75606ac626716b37a
mcalbuquerque <manoel.albuquerque@kellogg.northwestern.edu>
2022-02-21 21:56:21 +0000

    v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+      end
+    
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+      end
+    
+    
 end
